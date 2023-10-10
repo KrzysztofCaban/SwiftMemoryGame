2e1caf0e96d19864ffee69eeb044492a59cfe909
Krzysztof Caban <kcaban99@gmail.com>
2023-10-10 08:28:20 +0200

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Krzysztof Caban on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
